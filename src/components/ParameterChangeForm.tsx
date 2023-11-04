97e717b267c76b2eb1123648bb4e4aa80f585b88
Dan Connolly <dckc@madmode.com>
2023-11-04 12:19:39 -0500

    feat: show swingset params
     - use network config from context
     - render IST equivalent of beans

--- src/components/ParameterChangeForm.tsx
+++ src/components/ParameterChangeForm.tsx
@@ -1,18 +1,115 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { Button } from "./Button";
+import { useNetwork } from "../hooks/useNetwork";
 
 interface ParameterChangeFormProps {
   title: string;
   description: string;
 }
 
+type StringBeans = { key: string; beans: string };
+type PowerFlagFee = { power_flag: string; fee: Coins[] };
+type Coins = { denom: string; amount: string };
+type SwingsetParams = {
+  beans_per_unit: StringBeans[];
+  fee_unit_price: Coins[];
+  bootstrap_vat_config: string;
+  power_flag_fees: PowerFlagFee[];
+};
+
 const ParameterChangeForm: React.FC<ParameterChangeFormProps> = ({
   title,
   description,
 }) => {
+  const { networkConfig } = useNetwork();
+  const [params, setParams] = useState<SwingsetParams | null>(null);
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
+
+  useEffect(() => {
+    const getParams = async () => {
+      if (!networkConfig) return;
+      const content = await fetch(
+        `${networkConfig.api[0]}/agoric/swingset/params`
+      ).then((resp) => resp.text());
+      setParams(JSON.parse(content).params);
+    };
+    getParams();
+  }, [networkConfig]); // should also depend on a "governance epoch"
+  // that changes once per voting period / 2 (nyquist)
+
+  const showSwingsetPrice = async () => {
+    return false;
+  };
+
+  const renderCoins = (coins: Coins[]) => (
+    <td>{coins.map(({ denom, amount }) => `${amount} ${denom}`).join(",")}</td>
+  );
+
+  const renderFees = (fees: PowerFlagFee[]) => [
+    <tr>
+      <th>power_flag</th>
+      <th>fee</th>
+    </tr>,
+    ...fees.map(({ power_flag, fee }) => (
+      <tr>
+        <td>{power_flag}</td>
+        {renderCoins(fee)}
+      </tr>
+    )),
+  ];
+
+  const renderBeans = (items: StringBeans[]) => {
+    // We assume fee_unit_price remains 1IST
+    const { beans: feeUnit } = items.find(({ key }) => key === "feeUnit") || {};
+
+    return [
+      <thead>
+        <tr>
+          <th>key</th>
+          <th>beans</th>
+          <th>IST</th>
+        </tr>
+      </thead>,
+      <tbody>
+        {items.map(({ key, beans }) => (
+          <tr>
+            <td>{key}</td>
+            <td>{beans}</td>
+            <td>{Number(beans) / Number(feeUnit)}</td>
+          </tr>
+        ))}
+      </tbody>,
+    ];
+  };
+
+  const renderParams = (params: SwingsetParams) => (
+    <section>
+      <h3>Swingset Params</h3>
+
+      <table>
+        <tr>
+          <th>beans_per_unit</th>
+        </tr>
+        {renderBeans(params.beans_per_unit)}
+        <tr>
+          <th>fee_unit_price</th>
+        </tr>
+        <tr>{renderCoins(params.fee_unit_price)}</tr>
+        <tr>
+          <th>bootstrap_vat_config</th>
+        </tr>
+        <tr>
+          <td>{params.bootstrap_vat_config}</td>
+        </tr>
+        <tr>
+          <th>power_flag_fees</th>
+        </tr>
+        {renderFees(params.power_flag_fees)}
+      </table>
+    </section>
+  );
 
   return (
     <form className="py-6 px-8" onSubmit={onSubmit}>
@@ -28,9 +125,16 @@ const ParameterChangeForm: React.FC<ParameterChangeFormProps> = ({
           <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
             <div className="p-10 text-center">
               <span className="block text-sm leading-6 text-gray-900 sm:pt-1.5">
-                Coming Soon
+                <Button
+                  onClick={showSwingsetPrice}
+                  Icon={null}
+                  text="Swingset Storage Price"
+                  theme="light"
+                  layoutStyle="flex w-1/3"
+                />
               </span>
             </div>
+            {params ? renderParams(params) : []}
           </div>
         </div>
       </div>
