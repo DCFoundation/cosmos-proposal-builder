deb9ac18197219eba317daac6ce8ab705f7a4c46
Dan Connolly <dckc@madmode.com>
2023-11-04 14:29:58 -0500

    chore(param change): add title, description fields
     - copy Deposit field

--- src/components/ParameterChangeForm.tsx
+++ src/components/ParameterChangeForm.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
 import { Button } from "./Button";
 import { useNetwork } from "../hooks/useNetwork";
 
@@ -22,6 +22,8 @@ const ParameterChangeForm: React.FC<ParameterChangeFormProps> = ({
   description,
 }) => {
   const { networkConfig } = useNetwork();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descRef = useRef<HTMLTextAreaElement>(null);
   const [params, setParams] = useState<SwingsetParams | null>(null);
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -138,8 +140,69 @@ const ParameterChangeForm: React.FC<ParameterChangeFormProps> = ({
     </section>
   );
 
+  // TODO: factor out the title and description from ProposalForm
+  // er... or expand ProposalForm to handle ParamChange?
+
   return (
     <form className="py-6 px-8" onSubmit={onSubmit}>
+      <section>
+        <div className="space-y-12 sm:space-y-16">
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              {title}
+            </h2>
+            <p className="mt-1 max-w-3xl text-sm leading-6 text-gray-600">
+              {description}
+            </p>
+
+            <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+              <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
+                  Title
+                </label>
+                <div className="mt-2 sm:col-span-3 sm:mt-0">
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    placeholder="Title"
+                    ref={titleRef}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cardinal-600 sm:max-w-sm sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >
+                  Description
+                </label>
+                <div className="mt-2 sm:col-span-3 sm:mt-0">
+                  <textarea
+                    id="description"
+                    name="description"
+                    rows={3}
+                    className="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cardinal-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
+                    placeholder="Description"
+                    ref={descRef}
+                  />
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Write a few sentences about the proposal and include any
+                    relevant links.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <div className="space-y-12 sm:space-y-16">
         <div>
           <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -157,6 +220,33 @@ const ParameterChangeForm: React.FC<ParameterChangeFormProps> = ({
           </div>
         </div>
       </div>
+
+      <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
+        <label
+          htmlFor="description"
+          className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+        >
+          Deposit
+        </label>
+        <div className="mt-2 sm:col-span-3 sm:mt-0">
+          <input
+            type="number"
+            min="0"
+            step="1" // ensures integer
+            defaultValue="0"
+            name="deposit"
+            id="deposit"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cardinal-600 sm:max-w-sm sm:text-sm sm:leading-6"
+          />
+          <p className="mt-3 text-sm leading-6 text-gray-600">
+            A proposal requires{" "}
+            <span className="font-semibold">XXX10,000 ubld</span> to enter
+            voting period. Current balance{" "}
+            <span className="font-semibold">XX0 ubld</span>.
+          </p>
+        </div>
+      </div>
+
       <div className="mt-6 flex items-center justify-end gap-x-32">
         <Button
           onClick={proposeParamChange}
