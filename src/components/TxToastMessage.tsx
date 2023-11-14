e21eb88f7d0506b540c7712a29bb992f45b12fa6
0xPatrick <patrick@0xpatrick.dev>
2023-11-14 15:33:35 -0500

    feat: branding & colors

--- src/components/TxToastMessage.tsx
+++ src/components/TxToastMessage.tsx
@@ -73,7 +73,7 @@ export function TxToastMessage({
       <div className="ml-4 flex flex-shrink-0">
         <button
           type="button"
-          className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-cardinal-500 focus:ring-offset-2"
+          className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
           onClick={closeToast}
         >
           <span className="sr-only">Close</span>
