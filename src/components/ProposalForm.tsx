f807160699c3bdc14c7ec3c8d25a9cae6346c314
0xPatrick <patrick@0xpatrick.dev>
2024-01-08 22:38:47 -0500

    refactor(ui): make TitleDescriptionInputs a component

--- src/components/ProposalForm.tsx
+++ src/components/ProposalForm.tsx
@@ -14,6 +14,7 @@ import { ParameterChangeFormSection } from "./ParameterChangeForm";
 import { DepositSection } from "./DepositSection";
 import { paramOptions } from "../config/agoric/params";
 import type { ParameterChangeTypeOption } from "../types/form";
+import { TitleDescriptionInputs } from "./TitleDescriptionInputs";
 
 type BaseProposalArgs = {
   title: string;
@@ -112,54 +113,10 @@ const ProposalForm = forwardRef<ProposalFormMethods, ProposalFormProps>(
                   }
                 />
               ) : null}
-              <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="title"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Title
-                </label>
-                <div className="mt-2 sm:col-span-3 sm:mt-0">
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    placeholder="Title"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:max-w-sm sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
 
-              <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
-                <label
-                  htmlFor="description"
-                  className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
-                >
-                  Description
-                </label>
-                <div className="mt-2 sm:col-span-3 sm:mt-0">
-                  <textarea
-                    id="description"
-                    name="description"
-                    rows={3}
-                    className="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
-                    placeholder="Description"
-                  />
-                  <p className="mt-3 text-sm leading-6 text-gray-600">
-                    Write a few sentences about the proposal and include any
-                    relevant links. Before proposing to Mainnet, please ensure
-                    you've started a discussion on the{" "}
-                    <a
-                      className="cursor-pointer hover:text-gray-900 underline"
-                      href={governanceForumLink}
-                    >
-                      Community Forum
-                    </a>
-                    .
-                  </p>
-                </div>
-              </div>
+              <TitleDescriptionInputs
+                communityForumLink={governanceForumLink}
+              />
 
               {msgType === "coreEvalProposal" ? (
                 <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-4 sm:py-6">
